2b4baf38e714f61fc2ca6a9a19824834d556d648
Jeff Austin <jeffaustin81@gmail.com>
2015-09-23 14:36:36 -0700

    Add courses controller, courses factory, set ui router, set up index.html, home.html

---
+++ app.js
@@ -0,0 +1,14 @@
+var courseRoster = angular.module('courseRoster', ['ui.router']);
+
+courseRoster.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+  
+  $stateProvider.state('courses', {
+      url: "/courses",
+      templateUrl: "partials/courses.html",
+      controller: "CoursesCtrl"
+  });
+});
