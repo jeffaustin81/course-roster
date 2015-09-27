e42fac102b8f5b10459b1b3e39685f08c8daa974
Jeff Austin <jeffaustin81@gmail.com>
2015-09-26 23:42:02 -0700

    Add views header and body

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 var courseRoster = angular.module('courseRoster', ['ui.router']);
 
-courseRoster.config(function($stateProvider, $urlRouterProvider) {
+courseRoster.config(function($stateProvider) {
+
   $stateProvider.state('home', {
-    url: "",
-    templateUrl: "partials/home.html"
+    url: '',
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/home.html",
+      },
+    }
   });
-  
+
   $stateProvider.state('courses', {
-      url: "/courses",
-      templateUrl: "partials/courses.html",
-      controller: "CoursesCtrl"
+    url: "/courses",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/courses.html",
+        controller: 'CoursesCtrl'
+      },
+    }
   });
-  
-    $stateProvider.state('courses.students', {
+
+  $stateProvider.state('courses.students', {
     url: "/:courseId",
-    templateUrl: "partials/courses.students.html",
-    controller: 'StudentsCtrl'
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/courses.students.html",
+        controller: 'StudentsCtrl'
+      },
+    }
   });
 });
